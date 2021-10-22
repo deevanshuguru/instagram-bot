fc3b68bdeda5e664a0b798048ae020895bd1e366
Deevanshu Guru <51968560+deevanshuguru@users.noreply.github.com>
2021-10-23 00:25:47 +0530

    Create videoToStory.py

---
+++ videoToStory.py
@@ -0,0 +1,19 @@
+from instagrapi import Client
+from instagrapi.types import StoryMention, StoryMedia, StoryLink, StoryHashtag
+
+cl = Client()
+cl.login(USERNAME, PASSWORD, verification_code="<2FA CODE HERE>")
+
+media_pk = cl.media_pk_from_url('https://www.instagram.com/p/CTA31aPj3IH/')
+media_path = cl.video_download(media_pk)
+adw0rd = cl.user_info_by_username('deevanshu_guru')
+hashtag = cl.hashtag_info('hacktoberfest')
+
+cl.video_upload_to_story(
+    media_path,
+    "Credits deevanshu_guru",
+    mentions=[StoryMention(user=adw0rd, x=0.49892962, y=0.703125, width=0.8333333333333334, height=0.125)],
+    links=[StoryLink(webUri='https://github.com/adw0rd/instagrapi')],
+    hashtags=[StoryHashtag(hashtag=hashtag, x=0.23, y=0.32, width=0.5, height=0.22)],
+    medias=[StoryMedia(media_pk=media_pk, x=0.5, y=0.5, width=0.6, height=0.8)]
+)
